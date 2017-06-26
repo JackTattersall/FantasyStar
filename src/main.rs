6c32ed6315cadb7ef15decaa50fc92e2754e2245
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-06-26 21:03:02 +0100

    implemented move being in coord and tested outcomes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 #[allow(dead_code)]
 enum TerrainGround {
     Soil,
     Stone,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 #[allow(dead_code)]
 enum TerrainBlock {
     Tree,
@@ -13,14 +13,14 @@ enum TerrainBlock {
     Stone,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 #[allow(dead_code)]
 enum Being {
     Orc,
     Human,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 #[allow(dead_code)]
 enum Direction {
     West,
@@ -30,11 +30,16 @@ enum Direction {
 }
 
 #[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
 enum MovementError {
-    NoBeingInSquare
+    NoBeingInSquare,
+    SquareOutsideGridBoundary,
+    BeingAlreadyInSquare,
+    TerrainGroundUnsuitable,
 }
 
 #[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
 struct Square {
     ground: TerrainGround,
     block: Option<TerrainBlock>,
@@ -42,6 +47,7 @@ struct Square {
 }
 
 #[allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
 struct Grid {
     size: (usize, usize),
     squares: Vec<Square>,
@@ -52,7 +58,30 @@ impl Grid {
     fn move_being_in_coord(&self, coord: (usize, usize), direction: Direction) -> Result<(usize, usize), MovementError> {
         let square = self.squares.get(coord.0 * self.size.0 + coord.1).expect("Index out of bounds trying to get being");
         match square.beings {
-            Some(_) => Ok((0, 0)),
+            Some(_) => {
+                let mut coord_to_move_into: (usize, usize) = coord;
+                match direction {
+                    ::Direction::West => coord_to_move_into.1 -= 1,
+                    ::Direction::East => coord_to_move_into.1 += 1,
+                    ::Direction::South => coord_to_move_into.0 -= 1,
+                    ::Direction::North => coord_to_move_into.0 += 1,
+                };
+
+                if (coord_to_move_into.0 > (self.size.0 - 1)) |
+                     (coord_to_move_into.1 > (self.size.1 - 1)) {
+                        return Err(MovementError::SquareOutsideGridBoundary)
+                };
+
+                let square_moving_into: &Square = self.squares.get(coord_to_move_into.0 * self.size.0 + coord_to_move_into.1)
+                    .expect("Index out of bounds trying to get square_moving_into");
+
+                if square_moving_into.beings.is_some() { return Err(MovementError::BeingAlreadyInSquare)};
+
+                match square_moving_into.ground {
+                    ::TerrainGround::Soil => return Ok(coord_to_move_into),
+                    ::TerrainGround::Stone => return Err(MovementError::TerrainGroundUnsuitable)
+                };
+            }
             None => Err(MovementError::NoBeingInSquare)
         }
     }
@@ -79,6 +108,8 @@ impl Grid {
 
 // ------------------TESTS-------------------------
 
+// Grid tests
+
 #[cfg(test)]
 mod test {
     #[test]
@@ -96,6 +127,70 @@ mod test {
 
         assert_eq!(grid.squares.len(), 5 * 13);
         assert_eq!(number_of_squares, 5 * 13);
+    }
+
+    #[test]
+    fn try_moving_from_a_square_without_a_being() {
+        let grid = ::Grid::generate_empty(3, 3);
+        assert_eq!(
+            grid.move_being_in_coord((0, 0), ::Direction::West),
+            Err(::MovementError::NoBeingInSquare)
+        )
+    }
+
+    #[test]
+    fn try_moving_a_being_outside_grid() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        let test_being = ::Being::Orc;
+        let test_terrain_ground = ::TerrainGround::Soil;
+        let test_square = ::Square{ ground: test_terrain_ground, block: None, beings: Some(test_being)};
+        grid.squares.insert(8, test_square);
+
+        // Test moving east
+        assert_eq!(
+            grid.move_being_in_coord((2, 2), ::Direction::East),
+            Err(::MovementError::SquareOutsideGridBoundary)
+        );
+
+        // Test moving north
+        assert_eq!(
+            grid.move_being_in_coord((2, 2), ::Direction::North),
+            Err(::MovementError::SquareOutsideGridBoundary)
+        );
+
+        // south and west not tested as usize minimum value is 0
+    }
+
+    #[test]
+    fn being_already_exists_in_square_trying_to_move_into() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        let test_being = ::Being::Orc;
+        let test_terrain_ground = ::TerrainGround::Soil;
+        let test_square = ::Square{ ground: test_terrain_ground, block: None, beings: Some(test_being)};
+
+        grid.squares.insert(7, test_square.clone());
+        grid.squares.insert(8, test_square.clone());
+
+        assert_eq!(
+            grid.move_being_in_coord((2, 1), ::Direction::East),
+            Err(::MovementError::BeingAlreadyInSquare)
+        );
+    }
+
+    #[test]
+    fn try_to_move_being_to_stone_ground() {
+        let mut grid = ::Grid::generate_empty(3, 3);
+        let test_being = ::Being::Orc;
+        let test_terrain_ground = ::TerrainGround::Soil;
+        let test_square = ::Square{ ground: test_terrain_ground, block: None, beings: Some(test_being)};
+
+        grid.squares.insert(7, test_square);
+        grid.squares.insert(8, ::Square{ ground: ::TerrainGround::Stone, block: None, beings: None});
+
+        assert_eq!(
+            grid.move_being_in_coord((2, 1), ::Direction::East),
+            Err(::MovementError::TerrainGroundUnsuitable)
+        );
     }
 }
 
