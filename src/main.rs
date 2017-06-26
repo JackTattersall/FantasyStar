5464e383493ecd2ca6487c70c89da6ab8965f656
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-06-26 17:12:55 +0100

    created movement error and move_being_in_coord skeleton

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,20 @@ enum Being {
     Human,
 }
 
+#[derive(PartialEq, Debug)]
+#[allow(dead_code)]
+enum Direction {
+    West,
+    East,
+    North,
+    South,
+}
+
+#[allow(dead_code)]
+enum MovementError {
+    NoBeingInSquare
+}
+
 #[allow(dead_code)]
 struct Square {
     ground: TerrainGround,
@@ -34,6 +48,15 @@ struct Grid {
 }
 #[allow(dead_code)]
 impl Grid {
+
+    fn move_being_in_coord(&self, coord: (usize, usize), direction: Direction) -> Result<(usize, usize), MovementError> {
+        let square = self.squares.get(coord.0 * self.size.0 + coord.1).expect("Index out of bounds trying to get being");
+        match square.beings {
+            Some(_) => Ok((0, 0)),
+            None => Err(MovementError::NoBeingInSquare)
+        }
+    }
+
     fn generate_empty(size_x: usize, size_y: usize) -> Grid {
         let number_of_squares = size_x * size_y;
         let mut squares: Vec<Square> = Vec::with_capacity(number_of_squares);
@@ -71,8 +94,8 @@ mod test {
             number_of_squares += 1;
         }
 
-        assert_eq!(grid.squares.len(), 5*13);
-        assert_eq!(number_of_squares, 5*13);
+        assert_eq!(grid.squares.len(), 5 * 13);
+        assert_eq!(number_of_squares, 5 * 13);
     }
 }
 
